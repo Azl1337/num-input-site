b4e786cd0d1bfc9863217887f460e32ec563ca66
Azat Rakhmatullin <zzzzzz.yyyyyy.@zzzz.com>
2025-02-12 07:39:38 +0300

    modified: new file 404.html & modified any files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { NumberInputComponent } from './number-input/number-input.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: NumberInputComponent },
+];
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,19 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { ApplicationConfig } from '@angular/core';
 
-bootstrapApplication(AppComponent, {
-  providers: [provideAnimationsAsync()]
-}).catch((err) => console.error(err));
+function mergeConfig(baseConfig: ApplicationConfig, additionalProviders: any[]): ApplicationConfig {
+  return {
+    ...baseConfig,
+    providers: [
+      ...(baseConfig.providers || []),
+      ...additionalProviders,
+    ],
+  };
+}
+
+const mergedConfig = mergeConfig(appConfig, [provideAnimationsAsync()]);
+
+bootstrapApplication(AppComponent, mergedConfig).catch((err) => console.error(err));
